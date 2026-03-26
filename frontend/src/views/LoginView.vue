986a386631be818e7ddee1b0a59bdeaf98c092ed
Chichompf82 <marioferrarioas@gmail.com>
2026-03-26 13:22:49 -0300

    Modificación de host

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -31,7 +31,8 @@ const router = useRouter()
 
 const handleLogin = async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/auth/login', {
+    /* const res = await fetch('http://localhost:3000/api/auth/login', { */
+    const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -31,7 +31,8 @@ const router = useRouter()
 
 const handleRegister = async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/auth/register', {
+    /* const res = await fetch('http://localhost:3000/api/auth/register', { */
+    const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
