ea42a52c773090fa90280073ae0f41c13d49d7d2
Chichompf82 <marioferrarioas@gmail.com>
2026-04-26 18:58:27 -0300

    actualizacion URL del back en el front para conección 2

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -98,9 +98,8 @@ const handleLogin = async () => {
   loading.value = true
 
   try {
-    const res = await fetch('http://localhost:3000/api/auth/login', {
-      /* const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/login', { */
-
+    /* const res = await fetch('http://localhost:3000/api/auth/login', { */
+    const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -141,69 +140,3 @@ const goToRegister = () => {
   router.push('/register')
 }
 </script>
-
-<!-- <template>
-  <div class="container">
-    <div class="card">
-      <h1>🔐 Iniciar sesión</h1>
-
-      <form @submit.prevent="handleLogin" class="form">
-        <input v-model="email" type="email" placeholder="Email" class="input" required />
-        <input v-model="password" type="password" placeholder="Contraseña" class="input" required />
-        <button type="submit" class="btn">Ingresar</button>
-      </form>
-
-      <p v-if="error" class="error">{{ error }}</p>
-
-      <p class="link">
-        ¿No tenés cuenta?
-        <span @click="goToRegister">Registrarse</span>
-      </p>
-    </div>
-  </div>
-</template>
-
-<script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-
-const email = ref('')
-const password = ref('')
-const error = ref('')
-
-const router = useRouter()
-
-const handleLogin = async () => {
-  try {
-    const res = await fetch('http://localhost:3000/api/auth/login', {
-      /* const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/login', { */
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: email.value,
-        password: password.value,
-      }),
-    })
-
-    const data = await res.json()
-
-    if (!res.ok) {
-      throw new Error(data.message || 'Error al loguear')
-    }
-
-    // Guardar token en localStorage
-    localStorage.setItem('token', data.token)
-
-    // Redirigir a tareas
-    router.push('/tasks')
-  } catch (err) {
-    error.value = err.message
-  }
-}
-
-const goToRegister = () => {
-  router.push('/register')
-}
-</script> -->
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -95,8 +95,8 @@ const handleRegister = async () => {
   loading.value = true
 
   try {
-    const res = await fetch('http://localhost:3000/api/auth/register', {
-      /* const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/register', { */
+    /* const res = await fetch('http://localhost:3000/api/auth/register', { */
+    const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -135,70 +135,3 @@ const goToLogin = () => {
   router.push('/login')
 }
 </script>
-
-<!-- <template>
-  <div class="container">
-    <div class="card">
-      <h1>📝 Registro</h1>
-
-      <form @submit.prevent="handleRegister" class="form">
-        <input v-model="email" type="email" placeholder="Email" class="input" required />
-
-        <input v-model="password" type="password" placeholder="Contraseña" class="input" required />
-
-        <button type="submit" class="btn">Registrarse</button>
-      </form>
-
-      <p class="link">
-        ¿Ya tenés cuenta?
-        <span @click="goToLogin">Iniciar sesión</span>
-      </p>
-    </div>
-  </div>
-</template>
-
-<script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-
-const email = ref('')
-const password = ref('')
-const error = ref('')
-
-const router = useRouter()
-
-const handleRegister = async () => {
-  try {
-    const res = await fetch('http://localhost:3000/api/auth/register', {
-      /* const res = await fetch('https://task-manager-lux2.onrender.com/api/auth/register', { */
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: email.value,
-        password: password.value,
-      }),
-    })
-
-    const data = await res.json()
-
-    if (!res.ok) {
-      throw new Error(data.message || 'Error en el registro')
-    }
-
-    //guardar token
-    localStorage.setItem('token', data.token)
-
-    //redirigir
-    router.push('/tasks')
-  } catch (err) {
-    error.value = err.message
-  }
-}
-
-const goToLogin = () => {
-  router.push('/login')
-}
-</script>
- -->
